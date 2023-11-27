873c1b70be6c6058762f12b96eafb2ff64b400c3
nishujangra <ndjangra1027@gmail.com>
2023-11-28 02:26:07 +0530

    teaching dsp

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementsByClassName('heart')[0].addEventListener('click', function() {
+    document.getElementsByClassName('heart')[0].remove;
+    document.getElementsByClassName('container')[0].innerHTML = `
+            <h1>Thank you Mam, for teaching me DSP 💕</h1>
+    `;
+});
