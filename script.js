767afc2a3f462ec7e6378f44b93b454042918e42
nishujangra <ndjangra1027@gmail.com>
2023-11-29 23:32:38 +0530

    few lines

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 document.getElementsByClassName('heart')[0].addEventListener('click', function() {
     document.getElementsByClassName('heart')[0].remove;
     document.getElementsByClassName('container')[0].innerHTML = `
-            <h1> tum chutiya ho 💕</h1>
+            <h1> 
+                In the melody of life, you are my harmonious note, painting my world with joy. Your     laughter is a serenade, your eyes, constellations I get lost in. Each shared moment is a    cherished memory, and my love for you transcends time and distance. Until destiny unites   our hands, you are deeply and unequivocally loved.
+
+                <br><br><h1>Yours always, Nishant 💕</h1>
+            </h1>
     `;
 });
