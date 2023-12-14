790b592b4e02260701271c83cb0cdef5429e940d
nishujangra <ndjangra1027@gmail.com>
2023-12-14 23:57:44 +0530

    I love you

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ document.getElementsByClassName('heart')[0].addEventListener('click', function()
     document.getElementsByClassName('heart')[0].remove;
     document.getElementsByClassName('container')[0].innerHTML = `
             <h1> 
-                In the melody of life, you are my harmonious note, painting my world with joy. Your     laughter is a serenade, your eyes, constellations I get lost in. Each shared moment is a    cherished memory, and my love for you transcends time and distance. Until destiny unites   our hands, you are deeply and unequivocally loved.
-
+                I Love you ❤️
                 <br><br><h1>Yours always, Nishant 💕</h1>
             </h1>
     `;
